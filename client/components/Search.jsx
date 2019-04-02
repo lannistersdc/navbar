b83ca53e47082c10219989be459d6e24570bf51c
Liezel Manalo <liezelanne9@gmail.com>
2019-04-01 19:19:49 -0700

    [feat] Search Input Val and Find Table Button re-renders page (hardcode)

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -11,12 +11,34 @@ export default class Search extends Component {
       searchInput: ''
     }
     this.toggleSearch = this.toggleSearch.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFindTableButton = this.handleFindTableButton.bind(this);
   }
 
   toggleSearch(e) {
     e.preventDefault();
     let opened = !this.state.opened
-    this.setState({ opened }, () => console.log(`Search opened: ${this.state.opened}`))
+    this.setState({ 
+      opened 
+    }, () => console.log(`Search opened: ${this.state.opened}`))
+  }
+
+  handleInputChange(e) {
+    let searchInput = e.target.value;
+    // predictive search function goes here
+    this.setState({ 
+      searchInput 
+    }, () => console.log(this.state.searchInput))
+  }
+
+  handleFindTableButton(e) {
+    e.preventDefault();
+    // let id = e.target.id
+    // for now, hardcoded:
+    let restaurantId = this.props.restaurantId + 1;
+    this.props.fetchRestaurant(restaurantId);
+    // page rerenders based on restaurantId clicked
+
   }
 
   render() {
@@ -40,12 +62,11 @@ export default class Search extends Component {
               partySize
             </div>
 
-            <div>
-              search input
-            </div>
-
-            <div>
-              search button
+            <div id="search-input">
+              <form>
+                <input onChange={this.handleInputChange} value={this.state.searchInput} placeholder="Location, Restaurant, or Cuisine"></input><br />
+                <button name="find-table-btn" onClick={this.handleFindTableButton}>Find a Table</button>
+              </form>
             </div>
 
           </div>
