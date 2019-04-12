6b4f1ea57f318a7639db15a58405930c7da367d7
Liezel Manalo <liezelanne9@gmail.com>
2019-04-11 18:32:40 -0700

    Adjusted borders

--- client/components/Breadcrumb.jsx
+++ client/components/Breadcrumb.jsx
@@ -4,13 +4,13 @@ import styles from '../styles/breadcrumb.module.scss';
 const Breadcrumb = ({ region, city }) => {
   return (
     <div>
-      <ul className={styles.breadcrumb}>
+      <div className={styles.breadcrumb}>
         <li><a href="#">Home</a></li>
         <li>United States</li>
         <li><a href="#">Los Angeles</a></li>
         <li><a href="#">{region}</a></li>
         <li>{city}</li>
-      </ul>
+      </div>
     </div>
   )
 }
