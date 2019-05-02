7c971697e6499e10b65294bbc374cd8a239498d7
Anthony Kim <tony@Anthonys-MacBook-Pro.local>
2019-05-01 19:06:50 -0700

    [ops] changed database connection

--- database/index.js
+++ database/index.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://tony:password@52.53.248.33/navbar', () => console.log('~~~~~~~~~~ NavBar Database Connected ~~~~~~~~~~'))
+// mongoose.connect('mongodb://tony:password@52.53.248.33/navbar', () => console.log('~~~~~~~~~~ NavBar Database Connected ~~~~~~~~~~'))
+mongoose.connect('mongodb://tony:pass@54.67.66.252/navbar', () => console.log('~~~~~~~~~~ NavBar Database Connected ~~~~~~~~~~'))
+
 
 const navBarSchema = mongoose.Schema ({
   restaurantId: Number,
   restaurantName: String,
   restaurantCuisine: String,
-  location: String
+  location: { type: [String], index: true }
 })
 
-const Result = mongoose.model('results', navBarSchema)
+navBarSchema.index({location: "text"});
+
+const Result = mongoose.model('results', navBarSchema);
 
 module.exports = Result;
 
